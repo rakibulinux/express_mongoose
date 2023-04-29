ed3cc690bf5472fbae48ae3f3a04c0075ffa2747
Rakib Linux <rakibulinux@gmail.com>
2023-04-29 16:33:44 +0600

    schema added

--- index.js
+++ index.js
@@ -18,38 +18,87 @@ mongoose
   });
 
 // Schema
-const productSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please provide a name for this product"],
-    trim: true,
-    unique: true,
-    minLength: [3, "Name must be at lease 3 characters."],
-    maxLength: [100, "Name must be at lease 3 characters."],
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: [0, "Price can't be negative"],
-  },
-  unit: {
-    type: Number,
-    required: true,
-    enum: {
-      value: ["kg", "litre", "pcs"],
-      message: "Unit value can't be {VALUE}, must be kg/litle/pcs",
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name for this product"],
+      trim: true,
+      unique: true,
+      minLength: [3, "Name must be at lease 3 characters."],
+      maxLength: [100, "Name must be at lease 3 characters."],
     },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: [0, "Price can't be negative"],
+    },
+    unit: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["kg", "litre", "pcs"],
+        message: "Unit value can't be {VALUE}, must be kg/litle/pcs",
+      },
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: [0, "Qunatitly can't be negative"],
+      validate: {
+        validator: (value) => {
+          const isInteger = Number.isInteger(value);
+          if (isInteger) {
+            return true;
+          } else {
+            return false;
+          }
+        },
+      },
+      message: "Quantity must be an integer",
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["in-stock", "out-of-stock", "discontinued"],
+        message: "status can't be {VALUE}",
+      },
+    },
+    // createdAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // updatedAt: {
+    //   type: Date,
+    //   default: Date.now,
+    // },
+    // supplier: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: "Supplier",
+    // },
+    // categories: [
+    //   {
+    //     name: {
+    //       type: String,
+    //       require: true,
+    //     },
+    //     _id: mongoose.Schema.Types.ObjectId,
+    //   },
+    // ],
   },
-  quantity: {
-    type: Number,
-    required: true,
-    min: [0, "Qunatitly can't be negative"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+// SCHEMA -> MODEL -> QUERY
+
+const Product = mongoose.model("Product", productSchema);
 
 // Middleware Connections
 app.use(cors());
@@ -58,6 +107,38 @@ app.use(express.json());
 // Routes
 app.get("/", (req, res) => {
   res.send(`Server is running on port ${PORT}`);
+});
+
+app.post("/api/v1/product", async (req, res, next) => {
+  try {
+    // Save or Create
+
+    //Save methood
+    const product = new Product(req.body);
+
+    // Instent create -> Do something -> save()
+    if (product.quantity === 0) {
+      product.status = "out-of-stock";
+    }
+
+    const sendProduct = await product.save();
+
+    //Create methood
+    // const sendProduct = await Product.create(req.body);
+
+    res.status(200).json({
+      status: "success",
+      message: "Data inserted successfully",
+      data: sendProduct,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "Failed",
+      message: "Data is not inserted",
+      error: error.message,
+    });
+  }
+  //Save and create
 });
 
 // Connection
